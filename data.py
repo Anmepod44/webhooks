7d9165ae9abb36282a36258aeef3b4efdef415b7
DROID <mnyaanga44@gmail.com>
2024-08-29 15:39:29 -0400

    updated

--- data.py
+++ data.py
@@ -1,12 +1,14 @@
 import requests
 from openai import OpenAI
+import os
+
 
 client = OpenAI(
-    api_key="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"
+    api_key=os.getenv("OPENAI_KEY")
 )
 
 headers = {
-    'Authorization': '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',
+    'Authorization': os.getenv("GITHUB_ACCESS_TOKEN"),
     'X-GitHub-Api-Version': '2022-11-28'
 }
 
@@ -14,7 +16,8 @@ headers = {
 response = requests.get('https://api.github.com/repos/Anmepod44/webhooks/pulls', headers=headers)
 data = response.json()
 
-data=dict(data[0])
+
+print(f"{data} is the number of pull requests in the branch")
 
 def download_content(url):
     try:
