555976405af758acd5f2831c11f9483a61cd1cb6
zahlen <59273687+Anmepod44@users.noreply.github.com>
2024-08-29 09:10:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,5 @@ def greet():
 def feature_by_marvin():
     return "This feature was made by marvin"
 
-    return "Hello From Marvin"
-
 def feature_by_dev():
     return "dev made some changes here"
